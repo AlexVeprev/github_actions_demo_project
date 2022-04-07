faa3801560ee1c0c8179af92b6be9f096ead9bde
Alexey Veprev <aveprev27@gmail.com>
2022-04-07 15:06:25 +0300

    Add oneTBB

--- src/impl.cpp
+++ src/impl.cpp
@@ -1,7 +1,15 @@
+#include <oneapi/tbb.h>
+
 unsigned int get_uint_sum(unsigned int start, unsigned int end) {
-    unsigned int sum = 0;
-    for (unsigned int i = start; i < end; i++) {
-        sum += i;
+    return oneapi::tbb::parallel_reduce(oneapi::tbb::blocked_range<unsigned int>(start, end), 0,
+    [](oneapi::tbb::blocked_range<unsigned int> const& r, unsigned int init) -> unsigned int {
+        for (unsigned int v = r.begin(); v != r.end(); v++  ) {
+            init += v;
+        }
+        return init;
+    },
+    [](unsigned int lhs, unsigned int rhs) -> unsigned int {
+        return lhs + rhs;
     }
-    return sum;
+    );
 }
